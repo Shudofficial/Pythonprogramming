d8a5dc74556d0848d6485968fd67f0f52af71612
Samuel <samuelmsuu@gmail.com>
2023-11-19 11:03:50 +0300

    new file object

---
+++ operators.js
@@ -0,0 +1,15 @@
+// what are the final values of all the variables in the code below
+// let a = 1, b = 1;  // a = 2, b=2, c= 2, d= 1
+// let c = ++a,
+//     d = b++
+
+
+// What are the values of a and x after the code below?
+
+let a = 2;
+    
+let x = 1 + (a *= 2);    
+// The answer is:
+
+// a = 4 (multiplied by 2)
+// x = 5 (calculated as 1 + 4)
---
+++ variables.js
@@ -0,0 +1,7 @@
+let admin, name;
+    name = "john";
+    admin = name;
+alert( admin)    /* john */
+
+const ourplanet = "earth";
+let currentvisitor = "john"
