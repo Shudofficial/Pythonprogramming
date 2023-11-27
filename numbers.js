f226d73930a554701d798db847997dcb34c8ac85
Samuel <samuelmsuu@gmail.com>
2023-11-27 16:08:34 +0300

    Refactored code to use a function to read numbers from the user.

--- numbers.js
+++ numbers.js
@@ -1,6 +1,19 @@
-let num1 = prompt("Enter a Number", 0)
-let num2 = prompt("enter second number", 0)
-function sum () {
-    +num1 +num2
-}
-alert (sum )
+// let num1 = prompt("Enter a Number", 0)
+// let num2 = prompt("enter second number", 0)
+// function sum () {
+//     +num1 +num2
+// }
+// alert (sum )
+function readNumber() {
+    let num;
+  
+    do {
+      num = prompt("Enter a number please?", 0);
+    } while ( !isFinite(num) );
+  
+    if (num === null || num === '') return null;
+  
+    return +num;
+  }
+  
+  alert(`Read: ${readNumber()}`);
