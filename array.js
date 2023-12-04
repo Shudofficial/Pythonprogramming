82539b4dd50234e9d6c30b1788db4c20c29aae88
Samuel <samuelmsuu@gmail.com>
2023-12-04 10:47:41 +0300

    exercise on js arrays

---
+++ array.js
@@ -0,0 +1,39 @@
+// Let’s try 5 array operations.
+
+// Create an array styles with items “Jazz” and “Blues”.
+// Append “Rock-n-Roll” to the end.
+// Replace the value in the middle with “Classics”. Your code for finding the middle value should work for any arrays with odd length.
+// Strip off the first value of the array and show it.
+// Prepend Rap and Reggae to the array.
+let arr = ["Jazz", "Blues"]
+arr.push("Banana")
+arr[Math.floor((arr.length - 1) / 2)] = "Classics";
+alert( arr.shift())
+arr.unshift("Rap", "Reggae")
+// Write the function sumInput() that:
+
+// Asks the user for values using prompt and stores the values in the array.
+// Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+// Calculates and returns the sum of array items.
+function sumInput() {
+
+    let numbers = [];
+  
+    while (true) {
+  
+      let value = prompt("A number please?", 0);
+  
+      // should we cancel?
+      if (value === "" || value === null || !isFinite(value)) break;
+  
+      numbers.push(+value);
+    }
+  
+    let sum = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+  
+  alert( sumInput() );
